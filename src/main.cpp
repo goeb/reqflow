6fad78f00098bc9bd16ef7e2ee94057b4e857cb0
fred <fred@fred1>
2014-02-02 14:23:39 +0100

    command regex for testing regex

--- src/main.cpp
+++ src/main.cpp
@@ -254,6 +254,50 @@ int cmdConfig(int argc, const char **argv)
 
 int cmdRegex(int argc, const char **argv)
 {
+    if (argc != 2) usage();
+
+    regex_t regex;
+    int reti;
+
+    /* Compile regular expression */
+    reti = regcomp(&regex, argv[0], 0);
+    if (reti) {
+        LOG_ERROR("Could not compile regex");
+        return 1;
+    }
+
+    /* Execute regular expression */
+
+    const int N = 5;
+    regmatch_t pmatch[N];
+
+    char buffer[256];
+    const char *text = argv[1];
+    reti = regexec(&regex, text, N, pmatch, 0);
+    if (!reti) {
+        printf("Match: \n");
+        int i;
+        for (i=0; i<N; i++) {
+            if (pmatch[i].rm_so != -1) {
+                int length = pmatch[i].rm_eo - pmatch[i].rm_so;
+                memcpy(buffer, text+pmatch[i].rm_so, length);
+                buffer[length] = 0;
+                printf("match[%d]: %s\n", i, buffer);
+            } else printf("match[%d]:\n", i);
+        }
+    } else if (reti == REG_NOMATCH) {
+        printf("No match");
+
+    } else {
+        char msgbuf[100];
+        regerror(reti, &regex, msgbuf, sizeof(msgbuf));
+        LOG_ERROR("Regex match failed: %s", msgbuf);
+        return 1;
+    }
+
+    /* Free compiled regular expression if you want to use the regex_t again */
+    regfree(&regex);
+
     return 0;
 }
 
