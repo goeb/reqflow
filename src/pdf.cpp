adcf82b6de70cb77e969ba9fa1a1169b6d7caa98
Frederic Hoerni <fhoerni@free.fr>
2014-02-04 12:51:50 +0100

    sample code for parsing PDF text

---
+++ src/pdf.cpp
@@ -0,0 +1,20 @@
+
+// g++ pdf.cpp -lpoppler-cpp
+
+#include <iostream>
+
+#include <poppler/cpp/poppler-document.h>
+#include <poppler/cpp/poppler-page.h>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    poppler::document *doc = poppler::document::load_from_file(argv[1]);
+    const int pagesNbr = doc->pages();
+    cout << "page count: " << pagesNbr << endl;
+
+    for (int i = 0; i < pagesNbr; ++i)
+        cout << doc->create_page(i)->text().to_latin1().c_str() << endl;
+}
+
+
