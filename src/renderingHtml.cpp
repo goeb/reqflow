333a280f955d3504a6cbbc395d12e8bc61a499e6
fred <fred@fred1>
2014-02-06 23:10:54 +0100

    HTML coverage tables

--- src/renderingHtml.cpp
+++ src/renderingHtml.cpp
@@ -28,11 +28,15 @@ void htmlPrintHeader()
            "<style>\n"
            "body { font-family: Verdana,sans-serif; }\n"
            ".r_date { font-size: small; }\n"
+           ".r_path { font-size: small; }\n"
            ".r_errors { border: 1px solid #BBB; white-space: pre; font-bold; color: red; }\n"
+           ".r_warning { color: red; font-weight: bold; }\n"
            ".r_no_error { color: grey; }\n"
            "table { border-collapse:collapse; }\n"
-           "td.r_summary { text-align:right; border-bottom: 1px grey solid; }\n"
+           "td.r_summary { text-align:right; border-bottom: 1px grey solid; padding-left: 1em; }\n"
            "th.r_summary { text-align:center; padding-left: 1em; }\n"
+           "td.r_coverage { text-align:left; border-bottom: 1px grey solid; padding-left: 1em; }\n"
+           "th.r_coverage { text-align:left; padding-left: 1em; }\n"
            "</style>\n"
            "</head>\n"
            "<body>\n"
@@ -66,12 +70,14 @@ void htmlPrintErrors()
 
 void htmlPrintSummaryRow(const char *docId, int ratio, int covered, int total, const char *path)
 {
-    printf("<tr class=\"r_summary\"><td class=\"r_summary\">%s</td>"
+    const char *warning = "";
+    if (ratio != 100) warning = "r_warning";
+    printf("<tr class=\"r_summary %s\"><td class=\"r_summary\">%s</td>"
            "<td class=\"r_summary\">%d</td>"
            "<td class=\"r_summary\">%d</td>"
            "<td class=\"r_summary\">%d</td>"
            "<td class=\"r_summary\">%s</td></tr>\n",
-           docId, ratio, covered, total, path);
+           warning, docId, ratio, covered, total, path);
 
 }
 
@@ -104,7 +110,6 @@ void htmlPrintSummaryContents(const std::list<std::string> &documents)
 
 }
 
-
 void htmlPrintSummary(const std::list<std::string> &documents)
 {
     printf("<table class=\"r_summary\">");
@@ -120,6 +125,108 @@ void htmlPrintSummary(const std::list<std::string> &documents)
 
     printf("</table>");
 }
+
+void htmlPrintCoverageRow(const char *req1, const char *req2, const char *doc2Id, bool warning)
+{
+    const char *warningStyle = "";
+    if (warning) warningStyle = "r_warning";
+
+    printf("<tr class=\"r_coverage %s\">", warningStyle);
+    printf("<td class=\"r_coverage\">%s</td>", req1);
+    printf("<td class=\"r_coverage\">%s</td>", req2);
+    printf("<td class=\"r_coverage\">%s</td>", doc2Id);
+    printf("\n");
+}
+
+
+void htmlPrintCoverage(const Requirement &r, bool forward)
+{
+    if (forward) { // A covered by B
+        if (r.coveredBy.empty()) htmlPrintCoverageRow(r.id.c_str(), "", "", true);
+        else {
+            std::set<std::string>::iterator c;
+            FOREACH(c, r.coveredBy) {
+                const char* docId = 0;
+                bool warning = false;
+                Requirement *above = getRequirement(c->c_str());
+                if (!above) {
+                    docId = "Undefined";
+                    warning = true;
+                } else docId = above->parentDocumentId.c_str();
+
+                htmlPrintCoverageRow(r.id.c_str(), c->c_str(), docId, warning);
+            }
+        }
+    } else { // A covering B
+        if (r.covers.empty()) htmlPrintCoverageRow(r.id.c_str(), "", "", false);
+        else {
+            std::set<std::string>::iterator c;
+            FOREACH(c, r.covers) {
+                const char* docId = 0;
+                Requirement *ref = getRequirement(c->c_str());
+                if (!ref) docId = "Undefined";
+                else docId = ref->parentDocumentId.c_str();
+
+                htmlPrintCoverageRow(r.id.c_str(), c->c_str(), docId, false);
+            }
+        }
+
+    }
+}
+
+void htmlPrintMatrix(const ReqFileConfig &f, bool forward)
+{
+
+    printf("<h2 name=\"%s\">", f.id.c_str());
+    if (forward) printf("Forward Coverage");
+    else printf("Reverse Coverage");
+    printf(" for: %s</h2>\n", f.id.c_str());
+
+    printf("<div class=\"r_path\">%s</div>", f.path.c_str());
+
+    printf("<table class=\"r_coverage\">");
+    printf("<tr class=\"r_coverage\"><th class=\"r_coverage\">Requirements</th>");
+    if (forward) {
+        printf("<th class=\"r_coverage\">Requirements Above</th>");
+        printf("<th class=\"r_coverage\">Documents Above</th>");
+    } else {
+        // reverse
+        printf("<th class=\"r_coverage\">Requirements Underneath</th>");
+        printf("<th class=\"r_coverage\">Documents Underneath</th>");
+    }
+    printf("</tr>\n");
+
+    std::map<std::string, Requirement>::iterator r;
+    FOREACH(r, Requirements) {
+        if (r->second.parentDocumentId == f.id) {
+            htmlPrintCoverage(r->second, forward);
+        }
+    }
+    printf("</table>\n");
+}
+
+void htmlPrintCoverage(const std::list<std::string> documents, bool forward)
+{
+    std::list<std::string>::const_iterator docId;
+    FOREACH(docId, documents) {
+        std::map<std::string, ReqFileConfig>::const_iterator file = ReqConfig.find(*docId);
+        if (file == ReqConfig.end()) PUSH_ERROR("Invalid document id: %s", docId->c_str());
+        else htmlPrintMatrix(file->second, forward);
+    }
+}
+
+void htmlPrintAllCoverage(const std::list<std::string> &documents)
+{
+    // forward coverage (A covered by B)
+    printf("<div class=\"r_coverage\">");
+
+    htmlPrintCoverage(documents, true);
+
+    htmlPrintCoverage(documents, false);
+
+    printf("</div>");
+}
+
 
 void htmlPrintFooter()
 {
@@ -159,10 +266,10 @@ void htmlRender(int argc, const char **argv)
     // print dependencies between documents
 
     // print coverage for each document (A>B and A<B)
+    htmlPrintAllCoverage(documents);
 
     // print errors
     htmlPrintErrors();
-
 
     // print footer
     htmlPrintFooter();
