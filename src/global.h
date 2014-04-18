0f38699c6d279b91e47e999c98b55096d82859e1
Clément DAVID <clement.david@scilab-enterprises.com>
2014-04-18 17:06:04 +0200

    Switch the windows build to Autotools.
    Also compile main tests to non-installed binaries

--- src/global.h
+++ src/global.h
@@ -20,16 +20,4 @@
 
 #define FOREACH(var, container) for (var=container.begin(); var!= container.end(); var++)
 
-
-#ifdef _WIN32
-inline struct tm *localtime_r(const time_t *timep, struct tm *result)
-{
-    struct tm *lt = localtime(timep);
-    *result = *lt;
-    return result;
-}
-#endif
-
-
-
 #endif
