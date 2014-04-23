ad074a0e35dd5cdc1a3f29d541d493384c88af19
Clément DAVID <c.david86@gmail.com>
2014-04-23 17:40:20 +0200

    Fix some warnings on printf

--- src/ReqDocumentDocx.cpp
+++ src/ReqDocumentDocx.cpp
@@ -48,7 +48,7 @@ int ReqDocumentDocxXml::loadDocxXmlNode(xmlDocPtr doc, xmlNode *a_node, bool deb
         } else if (XML_TEXT_NODE == currentNode->type) {
             xmlChar *text;
             text = xmlNodeListGetRawString(doc, currentNode, 1);
-            LOG_DEBUG("text size: %d bytes", strlen((char*)text));
+            LOG_DEBUG("text size: %zd bytes", strlen((char*)text));
             LOG_DEBUG("text: %s", (char*)text);
 
             textInParagraphCurrent += (char*)text;
@@ -141,7 +141,7 @@ int ReqDocumentDocx::loadRequirements(bool debug)
         while ( (r = zip_fread(fileInZip, buffer, BUF_SIZ)) > 0) {
             contents.append(buffer, r);
         }
-        LOG_DEBUG("%s:%s: %d bytes", fileConfig->path.c_str(), CONTENTS, contents.size());
+        LOG_DEBUG("%s:%s: %zd bytes", fileConfig->path.c_str(), CONTENTS, contents.size());
 
         zip_fclose(fileInZip);
     } else {
